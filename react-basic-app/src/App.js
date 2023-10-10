a5338598c33988b88ead1b638ca3dd06ff6f95fb
Prateek Shukla <shuklaprateek@google.com>
2023-10-10 22:26:23 +0530

    basic project created

---
+++ react-basic-app/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>hello World 2!</h1>
+  );
+}
+
+export default App;
---
+++ vite-react-basic-app/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
